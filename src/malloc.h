e6aa1a63235f909118b6869f7b2359343c58aa6c
セコイア <sekoia2900@gmail.com>
2026-05-09 12:08:26 +0900

    Update malloc.h

--- src/malloc.h
+++ src/malloc.h
@@ -4,18 +4,18 @@
 #include <stddef.h>
 #include <stdint.h>
 
-// メモリブロックのヘッダー
 struct heap_chunk {
     size_t size;
     struct heap_chunk* next;
     int free;
 };
 
-// アライメントとサイズ定数
+// 全ファイル共通で使う「実体は外にあるよ」という宣言
+extern struct heap_chunk* heap_start;
+
 #define ALIGN(size) (((size) + (16 - 1)) & ~(16 - 1))
 #define HEADER_SIZE sizeof(struct heap_chunk)
 
-// 関数宣言
 void heap_init(void);
 void* kmalloc(size_t size);
 void kfree(void* ptr);
